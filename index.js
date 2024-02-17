ebee58e7bb72f51953f3f4544bc73ba42ad87e65
Fenila Philip <100304937+fenilaphilip@users.noreply.github.com>
2024-02-17 22:10:32 +0100

    showing total countries visited

--- index.js
+++ index.js
@@ -16,9 +16,23 @@ db.connect();
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-    res.render("index.ejs");
+let countries;
+app.get("/", async (req, res) => {
+    countries = await MarkCountriesVisted();
+    res.render("index.ejs",{
+        total: countries.length,
+    });
 });
+
+async function MarkCountriesVisted() {
+    let countries_visited = [];
+    const result = await db.query("SELECT country_code FROM visited_countries");
+    result.rows.forEach((country) => {
+        countries_visited.push(country.country_code);
+    });
+    console.log(countries_visited);
+    return countries_visited;
+}
 
 app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`);
