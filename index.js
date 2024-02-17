992efd6a90c2b45fb2d041e2dc7b409de7a53db5
Fenila Philip <100304937+fenilaphilip@users.noreply.github.com>
2024-02-17 21:19:57 +0100

    database added

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
 
 const app = express();
 const port = 3000;
+const db = new pg.Client({
+    user: "postgres",
+    host: "localhost",
+    database: "World",
+    password: process.env.db_pass,
+    port: 5432,
+});
+db.connect();
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
